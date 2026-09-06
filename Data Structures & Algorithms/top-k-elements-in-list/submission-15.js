f7df3701da09fdb907a6340bf28edc9c2296661d
gauravarcnet <gauravbansal0000@gmail.com>
2026-09-07 05:21:41 +0530

    Add: top-k-elements-in-list - submission-15

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-15.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let map = new Map();
+        for(let i = 0; i < nums.length; i++){
+            map.set(nums[i], (map.get(nums[i]) || 0)+1)
+        }
+    const sortedUniqueNums =        
+        Array.from(map.keys()).sort((a, b) => {
+        return map.get(b) - map.get(a);
+        });
+
+    return sortedUniqueNums.slice(0, k);  
+    }
+}
